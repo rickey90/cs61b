8d123622014ac83694b654a29dac6c9700338d30
Rickey Patel <rickey90@gmail.com>
2019-02-28 12:20:45 -0600

    Finished lab 1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -3,6 +3,20 @@
  */
 public class LeapYear {
 
+    /** Returns true if year is a leap year
+     *  @param  year to be analyzed
+     */
+    public static boolean isLeapYear(int year) {
+        if(year % 400 == 0) {
+            return true;
+        } else if((year % 4 == 0) && (year % 100 != 0)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
